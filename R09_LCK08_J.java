73e3255932f31f52716f7aa24f2b4bd2abe72778
Gati0420 <41804509+Gati0420@users.noreply.github.com>
2025-03-01 20:37:18 -0500

    Update R09_LCK08_J.java
    Compliant solution (finally Block)

--- R09_LCK08_J.java
+++ R09_LCK08_J.java
@@ -3,26 +3,27 @@
 */ 
 
 public final class Client {
-  private final Lock lock = new ReentrantLock();
+private final Lock lock = new ReentrantLock();
  
-  //Non-compliant code
-  public void doSomething(File file) {
-    InputStream in = null;
-    try {
-      in = new FileInputStream(file);
-      lock.lock();
-      // Perform operations on the open file
-    } catch (FileNotFoundException fnf) {
-      // Forward to handler
-    } finally {
-      lock.unlock();
-      if (in != null) {
+//Compliant code
+    public void doSomething(File file) {
+        InputStream in = null;
+        lock.lock();
         try {
-          in.close();
-        } catch (IOException e) {
-          // Forward to handler
+          in = new FileInputStream(file);
+            // Perform operations on the open file
+        } catch (FileNotFoundException fnf) {
+            // Forward to handler
+        } finally {
+            lock.unlock();
+            
+            if (in != null) {
+                try {
+                  in.close();
+                } catch (IOException e) {
+                  // Forward to handler
+                }
+            }
         }
-      }
     }
-  }
 }
