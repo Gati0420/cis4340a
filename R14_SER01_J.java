719cea059bdc352bd8b1fcb0a39c28698369b717
Gati0420 <41804509+Gati0420@users.noreply.github.com>
2025-03-01 20:40:30 -0500

    Update R14_SER01_J.java
    Compliant Solution (readObject(), writeObject())
    
    readObject() and writeObject() methods are instead declared private and non-static to limit their accessibility

--- R14_SER01_J.java
+++ R14_SER01_J.java
@@ -2,18 +2,19 @@
 //      SER01-J. Do not deviate from the proper signatures of serialization methods
 */ 
 
-//Non-compliant code
+//Compliant code
 public class Ser implements Serializable {
     private final long serialVersionUID = 123456789;
     private Ser() {
       // Initialize
     }
-    public static void writeObject(final ObjectOutputStream stream)
-      throws IOException {
+    private void writeObject(final ObjectOutputStream stream)
+        throws IOException {
       stream.defaultWriteObject();
     }
-    public static void readObject(final ObjectInputStream stream)
+     
+    private void readObject(final ObjectInputStream stream)
         throws IOException, ClassNotFoundException {
       stream.defaultReadObject();
     }
-  }
+}
